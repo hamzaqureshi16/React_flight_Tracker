8c0a25c35b93f15134e6b30dd31a4d94af526db8
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-08-12 02:05:45 +0500

    major bug fix

--- components/Mymap.jsx
+++ components/Mymap.jsx
@@ -10,6 +10,7 @@ import axios from 'axios';
 import markerIcon from '../src/assets/plane.png'; // Custom marker icon image
 import { toast, ToastContainer } from 'react-toastify';
 import { transformExtent } from 'ol/proj';
+import { useCallback } from 'react';
 
 export default function MapComponent() {
   const [markers, setMarkers] = useState([]); // State to hold the marker overlays
@@ -20,12 +21,10 @@ export default function MapComponent() {
 
   useEffect(()=>{
     console.log("extent",extent)
-
-    
-
   },[extent])
 
-  const fetchData = async () => {
+
+  const fetchData = useCallback(async () => {
     const username = `${import.meta.env.VITE_OPENSKY_USERNAME}`;
     const password = `${import.meta.env.VITE_OPENSKY_PASSWORD}}`;
     const credentials = `${username}:${password}`;
@@ -73,13 +72,65 @@ export default function MapComponent() {
 
         console.log(error);
       });
-  };
+  }, [extent]);
+
+  // const fetchData = async () => {
+  //   const username = `${import.meta.env.VITE_OPENSKY_USERNAME}`;
+  //   const password = `${import.meta.env.VITE_OPENSKY_PASSWORD}}`;
+  //   const credentials = `${username}:${password}`;
+  //   const basicAuthHeader = `Basic ${btoa(credentials)}`;
+  //   console.log('api call')
+  //   const url = `https://opensky-network.org/api/states/all?lomin=${extent[0]}&lomax=${extent[2]}&lamin=${extent[1]}&lamax=${extent[3]}`;
+  //   await axios.get(url, {
+  //     headers: {
+  //       Authorization: basicAuthHeader,
+  //     },
+  //   }).then((response) => {
+  //       const data = response.data.states;
+  //       console.log(data)
+  //       const newMarkers = data.map((item) => {
+  //         const [longitude, latitude, heading] = [item[5], item[6], item[10]];
+  //         const marker = new Overlay({
+  //           position: fromLonLat([longitude, latitude]),
+  //           positioning: 'center-center',
+  //           element: document.createElement('div'),
+  //           stopEvent: false,
+  //         });
+  //         const img = document.createElement('img');
+  //         img.src = markerIcon;
+  //         img.style.width = '30px';
+  //         img.style.height = '30px';
+  //         img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
+  //         const label = document.createElement('label');
+  //         label.textContent = item[1];
+  //         marker.getElement().appendChild(img);
+  //         marker.getElement().appendChild(label);
+  //         img.src = markerIcon;
+  //         img.style.width = '30px';
+  //         img.style.height = '30px';
+  //         img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
+  //         // const label = document.createElement('label');
+  //         label.textContent = item[1];
+  //         marker.getElement().appendChild(img);
+  //         marker.getElement().appendChild(label);
+  //         return marker;
+  //       });
+  //       setMarkers(newMarkers);
+  //     })
+  //     .catch((error) => {
+  //       toast.error('Error fetching data from OpenSky API');
+
+  //       console.log(error);
+  //     });
+  // };
 
   useEffect(() => {
-    fetchData();
-    const interval = setInterval(fetchData, 25000); // Call the API every 25 seconds
+    const interval = setInterval(() => {
+      fetchData();
+    }, 25000);
+  
     return () => clearInterval(interval);
-  }, []);
+  }, [extent, fetchData]);
 
   const mapRef = useRef(null);
 
