c81e7f3833058f0a8c2402af65d423d90349bd36
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-08-12 00:14:14 +0500

    optimized

--- components/Mymap.jsx
+++ components/Mymap.jsx
@@ -9,65 +9,73 @@ import { fromLonLat } from 'ol/proj';
 import axios from 'axios';
 import markerIcon from '../src/assets/plane.png'; // Custom marker icon image
 import { toast, ToastContainer } from 'react-toastify';
+import { transformExtent } from 'ol/proj';
 
 export default function MapComponent() {
   const [markers, setMarkers] = useState([]); // State to hold the marker overlays
   const [zoomLevel, setZoomLevel] = useState(5); // State to hold the zoom level
   const [center, setCenter] = useState(fromLonLat([73.0479, 33.6844])); // State to hold the center
+  const [extent, setExtent] = useState([ 43.604540625000006, 17.786665521455063, 102.491259375, 47.11223396185369 ]);
   
 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const username = `${import.meta.env.VITE_OPENSKY_USERNAME}`;
-      const password = `${import.meta.env.VITE_OPENSKY_PASSWORD}}`;
-      const credentials = `${username}:${password}`;
-      const basicAuthHeader = `Basic ${btoa(credentials)}`;
-
-      const url = 'https://opensky-network.org/api/states/all';
-      await axios.get(url, {
-        headers: {
-          Authorization: basicAuthHeader,
-        },
-      }).then((response) => {
-          const data = response.data.states;
-          const newMarkers = data.map((item) => {
-            const [longitude, latitude, heading] = [item[5], item[6], item[10]];
-            const marker = new Overlay({
-              position: fromLonLat([longitude, latitude]),
-              positioning: 'center-center',
-              element: document.createElement('div'),
-              stopEvent: false,
-            });
-            const img = document.createElement('img');
-            img.src = markerIcon;
-            img.style.width = '30px';
-            img.style.height = '30px';
-            img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
-            const label = document.createElement('label');
-            label.textContent = item[1];
-            marker.getElement().appendChild(img);
-            marker.getElement().appendChild(label);
-            img.src = markerIcon;
-            img.style.width = '30px';
-            img.style.height = '30px';
-            img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
-            // const label = document.createElement('label');
-            label.textContent = item[1];
-            marker.getElement().appendChild(img);
-            marker.getElement().appendChild(label);
-            return marker;
-          });
-          setMarkers(newMarkers);
-        })
-        .catch((error) => {
-          toast.error('Error fetching data from OpenSky API');
-
-          console.log(error);
-        });
-    };
+  useEffect(()=>{
+    console.log("extent",extent)
 
     
+
+  },[extent])
+
+  const fetchData = async () => {
+    const username = `${import.meta.env.VITE_OPENSKY_USERNAME}`;
+    const password = `${import.meta.env.VITE_OPENSKY_PASSWORD}}`;
+    const credentials = `${username}:${password}`;
+    const basicAuthHeader = `Basic ${btoa(credentials)}`;
+    console.log('api call')
+    const url = `https://opensky-network.org/api/states/all?lomin=${extent[0]}&lomax=${extent[2]}&lamin=${extent[1]}&lamax=${extent[3]}`;
+    await axios.get(url, {
+      headers: {
+        Authorization: basicAuthHeader,
+      },
+    }).then((response) => {
+        const data = response.data.states;
+        console.log(data)
+        const newMarkers = data.map((item) => {
+          const [longitude, latitude, heading] = [item[5], item[6], item[10]];
+          const marker = new Overlay({
+            position: fromLonLat([longitude, latitude]),
+            positioning: 'center-center',
+            element: document.createElement('div'),
+            stopEvent: false,
+          });
+          const img = document.createElement('img');
+          img.src = markerIcon;
+          img.style.width = '30px';
+          img.style.height = '30px';
+          img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
+          const label = document.createElement('label');
+          label.textContent = item[1];
+          marker.getElement().appendChild(img);
+          marker.getElement().appendChild(label);
+          img.src = markerIcon;
+          img.style.width = '30px';
+          img.style.height = '30px';
+          img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
+          // const label = document.createElement('label');
+          label.textContent = item[1];
+          marker.getElement().appendChild(img);
+          marker.getElement().appendChild(label);
+          return marker;
+        });
+        setMarkers(newMarkers);
+      })
+      .catch((error) => {
+        toast.error('Error fetching data from OpenSky API');
+
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
     fetchData();
     const interval = setInterval(fetchData, 25000); // Call the API every 25 seconds
     return () => clearInterval(interval);
@@ -103,6 +111,11 @@ export default function MapComponent() {
       const newCenter = map.getView().getCenter();
       if (newCenter !== center) {
         setCenter(newCenter);
+      }
+      const newExtent = map.getView().calculateExtent(map.getSize());
+      if (newExtent !== extent) {
+        const transformedExtent = transformExtent(newExtent, 'EPSG:3857', 'EPSG:4326');
+        setExtent(transformedExtent);
       }
     });
 
--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import MyMap from '../components/Mymap'
-
+import plane from './assets/plane.png'
 
 function App() { 
 
   useEffect(() => {
-    document.title = "Flight Tracker"
-    console.log(import.meta.env)
+    document.title = "Flight Tracker" 
+    const link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      const link = document.createElement('link');
+      link.rel = 'icon';
+
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
+
   }, [])
 
 
