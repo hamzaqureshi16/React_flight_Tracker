72b473ba22362fe91d894c42e5c137c2f3d75df9
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-08-13 12:02:03 +0500

    added some details

--- components/Mymap.jsx
+++ components/Mymap.jsx
@@ -17,7 +17,7 @@ export default function MapComponent() {
   const [zoomLevel, setZoomLevel] = useState(5); // State to hold the zoom level
   const [center, setCenter] = useState(fromLonLat([73.0479, 33.6844])); // State to hold the center
   const [extent, setExtent] = useState([ 43.604540625000006, 17.786665521455063, 102.491259375, 47.11223396185369 ]);
-  const [queryType, setQueryType] = useState('icao24'); 
+  const [queryType, setQueryType] = useState(0); 
   const [query, setQuery] = useState('');
 
 
@@ -65,7 +65,24 @@ export default function MapComponent() {
           img.style.height = '30px';
           img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
           // const label = document.createElement('label');
-          label.textContent = item[1];
+          label.textContent = `${item[1]}`;
+
+          //make an icao label
+          const icaoLabel = document.createElement('label');
+          icaoLabel.textContent = `ICAO: ${item[0]}`;
+          icaoLabel.style.display = 'none';
+          marker.getElement().appendChild(icaoLabel);
+          
+          
+
+          marker.getElement().onmouseover = function () {
+            icaoLabel.style.display = 'block';
+          };
+          marker.getElement().onmouseout = function () {
+            icaoLabel.style.display = 'none';
+          };
+
+
           marker.getElement().appendChild(img);
           marker.getElement().appendChild(label);
           return marker;
@@ -78,56 +95,6 @@ export default function MapComponent() {
         console.log(error);
       });
   }, [extent]);
-
-  // const fetchData = async () => {
-  //   const username = `${import.meta.env.VITE_OPENSKY_USERNAME}`;
-  //   const password = `${import.meta.env.VITE_OPENSKY_PASSWORD}}`;
-  //   const credentials = `${username}:${password}`;
-  //   const basicAuthHeader = `Basic ${btoa(credentials)}`;
-  //   console.log('api call')
-  //   const url = `https://opensky-network.org/api/states/all?lomin=${extent[0]}&lomax=${extent[2]}&lamin=${extent[1]}&lamax=${extent[3]}`;
-  //   await axios.get(url, {
-  //     headers: {
-  //       Authorization: basicAuthHeader,
-  //     },
-  //   }).then((response) => {
-  //       const data = response.data.states;
-  //       console.log(data)
-  //       const newMarkers = data.map((item) => {
-  //         const [longitude, latitude, heading] = [item[5], item[6], item[10]];
-  //         const marker = new Overlay({
-  //           position: fromLonLat([longitude, latitude]),
-  //           positioning: 'center-center',
-  //           element: document.createElement('div'),
-  //           stopEvent: false,
-  //         });
-  //         const img = document.createElement('img');
-  //         img.src = markerIcon;
-  //         img.style.width = '30px';
-  //         img.style.height = '30px';
-  //         img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
-  //         const label = document.createElement('label');
-  //         label.textContent = item[1];
-  //         marker.getElement().appendChild(img);
-  //         marker.getElement().appendChild(label);
-  //         img.src = markerIcon;
-  //         img.style.width = '30px';
-  //         img.style.height = '30px';
-  //         img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
-  //         // const label = document.createElement('label');
-  //         label.textContent = item[1];
-  //         marker.getElement().appendChild(img);
-  //         marker.getElement().appendChild(label);
-  //         return marker;
-  //       });
-  //       setMarkers(newMarkers);
-  //     })
-  //     .catch((error) => {
-  //       toast.error('Error fetching data from OpenSky API');
-
-  //       console.log(error);
-  //     });
-  // };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -215,7 +182,13 @@ export default function MapComponent() {
           img.style.height = '30px';
           img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
           const label = document.createElement('label');
-          label.textContent = result[1];
+          label.textContent = `${result[1]} 
+          ICAO: ${result[0]} 
+          Altitude: ${result[7]} ft 
+          Velocity: ${result[9]} m/s 
+          Vertical Rate: ${result[11]} m/s 
+          Callsign: ${result[1]}
+          `;
           marker.getElement().appendChild(img);
           marker.getElement().appendChild(label);
           img.src = markerIcon;
@@ -223,9 +196,9 @@ export default function MapComponent() {
           img.style.height = '30px';
           img.style.transform = `rotate(${heading}deg)`; // Rotate the image based on the heading value
           // const label = document.createElement('label');
-          label.textContent = result[1];
           marker.getElement().appendChild(img);
           marker.getElement().appendChild(label);
+
 
           setMarkers([marker]);
           setCenter(fromLonLat([longitude, latitude]));
@@ -262,7 +235,7 @@ export default function MapComponent() {
           onChange={(e)=>{
           setQueryType(e.target.value)
           }}
-          defaultValue={0}
+          value={queryType}
         >
 
           <option 
@@ -307,6 +280,7 @@ export default function MapComponent() {
           setQueryType(0)
           setMarkers([])
           setExtent(extent);
+          fetchData();
         }}
 
         />
